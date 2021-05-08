17b6043dc3d2bd290935ea96867c758c6b26bf8a
Inna3112 <innula3112@gmail.com>
2021-05-08 18:23:30 +0300

    Changed App: install localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from './Component/Counter';
 import {SettingsBlock} from "./Component/SettingsBlock";
@@ -7,6 +7,31 @@ function App() {
     const [num, setNum] = useState<number>(0)
     const [minValue, setMinValue] = useState<number>(0)
     const [maxValue, setMaxValue] = useState<number>(0)
+
+    useEffect(() => {
+        let numAsString = localStorage.getItem('num')
+        if(numAsString){
+            let newNum = JSON.parse(numAsString)
+            setNum(newNum)
+        }
+        let minValueAsString = localStorage.getItem('minValue')
+        if(minValueAsString){
+            let newMinValue = JSON.parse(minValueAsString)
+            setMinValue(newMinValue)
+        }
+        let maxValueAsString = localStorage.getItem('maxValue')
+        if(maxValueAsString){
+            let newMaxValue = JSON.parse(maxValueAsString)
+            setMaxValue(newMaxValue)
+        }
+
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem('num', JSON.stringify(num))
+        localStorage.setItem('minValue', JSON.stringify(minValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+    }, [num, minValue, maxValue])
 
     function increaseInc(maxValue: number) {
         if (num < maxValue)
